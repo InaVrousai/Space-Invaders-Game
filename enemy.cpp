8a4dea8adbb333fae9bdc22d8652d8f8d9f00633
MateoMita533 <mateomita220@gmail.com>
2025-01-06 21:58:35 +0200

    merikes idees gia enemy den mporesa na ta treksw

--- enemy.cpp
+++ enemy.cpp
@@ -1,21 +1,63 @@
 #include "enemy.h"
+#include <sgg/graphics.h>
 
 void Enemy::init()
 {
+	setPosX(en_x);
+	setPosY(en_y);
+
+	brush_en.fill_opacity = 1.0f;
+	brush_en.texture = "";
 }
 
 void Enemy::draw()
 {
+	graphics::drawRect(getPosX(), getPosY(), 1.0f, 1.0f, brush_en);
 }
 
-void Enemy::update(float dt) 
-{
-}
-
-void takeDamage(int damage) 
+void Enemy::takeDamage(int damage)
 {
 	if (health >= damage)
 		health -= damage;
 	else
 		health = 0;
+}
+
+Enemy::Enemy(GameState* gs, const std::string& name)
+	:GameObject(gs,name)
+{
+	//skefthka na arxikopoihte h thesh twn enemies edw
+	en_x = 5.0f;
+	en_y = 5.0f;
+}
+
+void Enemy::update(float dt)
+{
+	//gia ena xroniko diasthma na phgainei aristera,gia to idio panw klp.
+	while (dt <= 50)
+		if (dt <= 10)
+			x += x * speed;
+		else if (dt <= 20)
+			speed += 0.1f;
+			x -= x * speed;
+		else if(dt<=40)
+			speed += 0.1f;
+			y -= y * speed;
+		else
+			speed -= 0.1f;
+			y += y * speed;
+
+	if (isDead())
+		//delete?
+	//Boundary check:
+	/*if (x < 0 || x > m_state->getCanvasWidth() ||
+		y < 0 || y > m_state->getCanvasHeight()) {
+		setActive(false); // Deactivate the enemy
+	}*/
+	//takeDamage otan ton petyxainei bulletPlayer
+	/*
+	* if bullet gets enemy call takeDamage
+	*/
+			/*
+			*/
 }
--- enemy.h
+++ enemy.h
@@ -1,17 +1,25 @@
-//#pragma once
-//#include "GameObject.h"
-//
-//class Enemy : public GameObject {
-//    float x, y;     // Enemy's position
-//    float speed;    // Movement speed
-//    int health;     // Enemy health points
-//
-//public:
-//    Enemy(GameState* gs, const std::string& name, float initX, float initY, float initSpeed, int initHealth)
-//        : GameObject(gs, name), x(initX), y(initY), speed(initSpeed), health(initHealth) {}
-//	  void init() override
-//    void update(float dt) override; // Update enemy state
-//    void draw() override;           // Render enemy
-//    bool isDead() const { return health <= 0; } // Check if enemy is dead
-//    void takeDamage(int damage);    // Reduce health when hit
-//};
+#pragma once
+#include "GameObject.h"
+#include <sgg/graphics.h>
+
+class Enemy : public GameObject,public Collision {
+    float x, y;     // Enemy's position
+    float speed = 0.1f;    // Movement speed
+    int health;     // Enemy health points
+    graphics::Brush brush_en;
+
+public:
+    //Enemy(GameState* gs, const std::string& name, float initX, float initY, float initSpeed, int initHealth)
+      //  : GameObject(gs, name), x(initX), y(initY), speed(initSpeed), health(initHealth) {}
+	
+    Enemy(GameState* gs, const std::string& name);
+    void init() override
+    void update(float dt) override; // Update enemy state
+    void draw() override;           // Render enemy
+    bool isDead() const { return health <= 0; } // Check if enemy is dead
+    void takeDamage(int damage);    // Reduce health when hit
+    float getPosX() { return this->x; }
+    float getPosY() { return this->y; }
+    void setPosX(float x) { this->x = x; }
+    void setPosY(float y) { this->y = y; }
+};
