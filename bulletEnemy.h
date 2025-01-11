df739741ca2e00590ef2196516edf46c4f8f3eed
MateoMita533 <mateomita220@gmail.com>
2025-01-11 17:59:52 +0200

    ghf

--- bulletEnemy.h
+++ bulletEnemy.h
@@ -1,18 +1,18 @@
 #pragma once
-#include <sgg/graphics.h>
+#include "sgg/graphics.h"
 #include "enemy.h"
 
-class bulletEnemy:public GameObject
+class bulletEnemy :public GameObject
 {
 public:
 	bulletEnemy(GameState* gs, const std::string& name);
-	void fireBullet();
-	void init() override;
+
+	void init(float x, float y) override;
 	void draw() override;
 	void update(float dt) override;
 
-private:
-	float speed;
+protected:
+	float speed = 7.0f;
 	float x;
 	float y;
 	graphics::Brush bulletBrush;
